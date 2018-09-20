108a9df11fac1c2954ed6efea51e0f2b02071dec
ihatethissystem <43032279+ihatethissystem@users.noreply.github.com>
2018-09-20 15:21:42 +0530

    Add files via upload

---
+++ add2.py
@@ -0,0 +1,27 @@
+>>> a=12
+>>> b=23
+>>> c=34
+>>> d=(a+b+c)
+>>> print(d)
+69
+>>> a=11
+>>> b=34
+>>> c=55
+>>> d=(a+b+c)
+>>> pr
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'pr' is not defined
+>>> a=12>23
+>>> print(a)
+False
+>>> a=67
+>>> b=56
+>>> c=67
+>>> d=(a/b*c)
+>>> print(d)
+67
+>>> a=(12/21*45+46)
+>>> print(a)
+46
+
