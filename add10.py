4f240e87051a42c303454fd8d25cf63106810a21
ihatethissystem <43032279+ihatethissystem@users.noreply.github.com>
2018-09-22 10:23:49 +0530

    Add files via upload

---
+++ add10.py
@@ -0,0 +1,496 @@
+dl320@soetcse:~/udaya$ python3 add6.py
+enter the  value of a56
+enter the value of b65
+enter the value of c67
+c is greater than b
+dl320@soetcse:~/udaya$ python3 add6.py
+enter the  value of a01
+enter the value of b69
+enter the value of c96
+c is greater than b
+dl320@soetcse:~/udaya$ python3 add6.py
+enter the  value of a87
+enter the value of b87
+enter the value of c98
+c is greater than b
+
+dl320@soetcse:~/udaya$ python3 add8.py
+0
+1
+a is even number
+2
+3
+a is even number
+4
+5
+a is even number
+6
+7
+a is even number
+8
+9
+a is even number
+10
+11
+a is even number
+12
+13
+a is even number
+14
+15
+a is even number
+16
+17
+a is even number
+18
+19
+a is even number
+20
+21
+a is even number
+22
+23
+a is even number
+24
+25
+a is even number
+26
+27
+a is even number
+28
+29
+a is even number
+30
+31
+a is even number
+32
+33
+a is even number
+34
+35
+a is even number
+36
+37
+a is even number
+38
+39
+a is even number
+40
+41
+a is even number
+42
+43
+a is even number
+44
+45
+a is even number
+46
+47
+a is even number
+48
+49
+a is even number
+50
+51
+a is even number
+52
+53
+a is even number
+54
+55
+a is even number
+56
+57
+a is even number
+58
+59
+a is even number
+60
+61
+a is even number
+62
+63
+a is even number
+64
+65
+a is even number
+66
+67
+a is even number
+68
+69
+a is even number
+70
+71
+a is even number
+72
+73
+a is even number
+74
+75
+a is even number
+76
+77
+a is even number
+78
+79
+a is even number
+80
+81
+a is even number
+82
+83
+a is even number
+84
+85
+a is even number
+86
+87
+a is even number
+88
+89
+a is even number
+90
+91
+a is even number
+92
+93
+a is even number
+94
+95
+a is even number
+96
+97
+a is even number
+98
+99
+a is even number
+100
+101
+a is even number
+102
+103
+a is even number
+104
+105
+a is even number
+106
+107
+a is even number
+108
+109
+a is even number
+110
+111
+a is even number
+112
+113
+a is even number
+114
+115
+a is even number
+116
+117
+a is even number
+118
+119
+a is even number
+120
+121
+a is even number
+122
+123
+a is even number
+124
+125
+a is even number
+126
+127
+a is even number
+128
+129
+a is even number
+130
+131
+a is even number
+132
+133
+a is even number
+134
+135
+a is even number
+136
+137
+a is even number
+138
+139
+a is even number
+140
+141
+a is even number
+142
+143
+a is even number
+144
+145
+a is even number
+146
+147
+a is even number
+148
+149
+a is even number
+150
+151
+a is even number
+152
+153
+a is even number
+154
+155
+a is even number
+156
+157
+a is even number
+158
+159
+a is even number
+160
+161
+a is even number
+162
+163
+a is even number
+164
+165
+a is even number
+166
+167
+a is even number
+168
+169
+a is even number
+170
+171
+a is even number
+172
+173
+a is even number
+174
+175
+a is even number
+176
+177
+a is even number
+178
+179
+a is even number
+180
+181
+a is even number
+182
+183
+a is even number
+184
+185
+a is even number
+186
+187
+a is even number
+188
+189
+a is even number
+190
+191
+a is even number
+192
+193
+a is even number
+194
+195
+a is even number
+196
+197
+a is even number
+198
+199
+a is even number
+200
+201
+a is even number
+202
+203
+a is even number
+204
+205
+a is even number
+206
+207
+a is even number
+208
+209
+a is even number
+210
+211
+a is even number
+212
+213
+a is even number
+214
+215
+a is even number
+216
+217
+a is even number
+218
+219
+a is even number
+220
+221
+a is even number
+222
+223
+a is even number
+224
+225
+a is even number
+226
+227
+a is even number
+228
+229
+a is even number
+230
+231
+a is even number
+232
+233
+a is even number
+234
+235
+a is even number
+236
+237
+a is even number
+238
+239
+a is even number
+240
+241
+a is even number
+242
+243
+a is even number
+244
+245
+a is even number
+246
+247
+a is even number
+248
+249
+a is even number
+250
+a is odd number
+
+
+
+dl320@soetcse:~/udaya$ python3 add9.py
+29
+dl320@soetcse:~/udaya$ python3 add9.py
+26
+dl320@soetcse:~/udaya$ python3 add9.py
+29
+dl320@soetcse:~/udaya$ ^C
+dl320@soetcse:~/udaya$ python3 add9.py
+31
+dl320@soetcse:~/udaya$ python3 add9.py
+34
+dl320@soetcse:~/udaya$ python3 add9.py
+27
+dl320@soetcse:~/udaya$ python3 add9.py
+27
+dl320@soetcse:~/udaya$ python3 add9.py
+33
+dl320@soetcse:~/udaya$ python3 add9.py
+33
+dl320@soetcse:~/udaya$ python3 add9.py
+31
+dl320@soetcse:~/udaya$ python3 add8.py
+dl320@soetcse:~/udaya$ python3 add9.py
+27
+dl320@soetcse:~/udaya$ python3 add9.py
+34
+dl320@soetcse:~/udaya$ python3 add9.py
+29
+dl320@soetcse:~/udaya$ python3 add9.py
+28
+dl320@soetcse:~/udaya$ python3 add9.py
+31
+dl320@soetcse:~/udaya$ python3 add9.py
+25
+dl320@soetcse:~/udaya$ 
+dl320@soetcse:~/udaya$ python3 add9.py
+31
+
+
+dl320@soetcse:~/udaya$ python3 add9.py
+12
+dl320@soetcse:~/udaya$ python3 add9.py
+1
+dl320@soetcse:~/udaya$ mkdir udaya
+dl320@soetcse:~/udaya$ python3 add9.py
+12
+dl320@soetcse:~/udaya$ python3 add9.py
+9
+dl320@soetcse:~/udaya$ python3 add9.py
+15
+dl320@soetcse:~/udaya$ python3 add9.py
+18
+dl320@soetcse:~/udaya$ python3 add9.py
+13
+dl320@soetcse:~/udaya$ python3 add9.py
+13
+dl320@soetcse:~/udaya$ python3 add9.py
+11
+dl320@soetcse:~/udaya$ python3 add9.py
+6
+dl320@soetcse:~/udaya$ python3 add9.py
+12
+dl320@soetcse:~/udaya$ python3 add9.py
+20
+dl320@soetcse:~/udaya$ python3 add9.py
+
+
+dl320@soetcse:~/udaya$ python3 add10.py
+enter q to quit and r to roll the diser
+6
+dl320@soetcse:~/udaya$ python3 add10.py
+enter q to quit and r to roll the diseq
+bye
+dl320@soetcse:~/udaya$ python3 add10.py
+enter q to quit and r to roll the diser
+6
+dl320@soetcse:~/udaya$ python3 add10.py
+enter q to quit and r to roll the diser
+6
+dl320@soetcse:~/udaya$ python3 add10.py
+enter q to quit and r to roll the diseq
+bye
+dl320@soetcse:~/udaya$ python3 add10.py
+enter q to quit and r to roll the diser
+4
+dl320@soetcse:~/udaya$ python3 add10.py
+enter q to quit and r to roll the diser
+3
+dl320@soetcse:~/udaya$ python3 add10.py
+enter q to quit and r to roll the diseq    
+bye
+dl320@soetcse:~/udaya$ python3 add10.py
+enter q to quit and r to roll the diser
+4
+
+
+
+
+
