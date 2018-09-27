a13ffc9210bf8aa633f065d5a4454f0d4a9a8a38
ihatethissystem <43032279+ihatethissystem@users.noreply.github.com>
2018-09-27 16:26:09 +0530

    Add files via upload

---
+++ snakeandladder5.py
@@ -0,0 +1,78 @@
+dl320@soetcse:~/udaya$ python3 snakeandladder4.py
+enter r or qr
+dice number and the number you are on is 6 6
+enter r or qr
+dice number and the number you are on is 2 8
+u have got a ladder, clim it up
+enter r or qr
+dice number and the number you are on is 1 38
+u r caught by a snake, go down
+enter r or qr
+dice number and the number you are on is 2 11
+u r caught by a snake, go down
+enter r or qr
+dice number and the number you are on is 1 3
+enter r or qr           
+dice number and the number you are on is 5 8
+u have got a ladder, clim it up
+enter r or qr
+dice number and the number you are on is 6 43
+enter r or qr
+dice number and the number you are on is 4 47
+enter r or qr
+dice number and the number you are on is 6 53
+enter r or qr
+dice number and the number you are on is 3 56
+enter r or qr
+dice number and the number you are on is 1 57
+enter r or qr
+dice number and the number you are on is 4 61
+enter r or qr
+dice number and the number you are on is 6 67
+enter r or qr
+dice number and the number you are on is 2 69
+enter r or qr
+dice number and the number you are on is 3 72
+enter r or qr
+dice number and the number you are on is 3 75
+enter r or qr
+dice number and the number you are on is 4 79
+enter r or qr
+dice number and the number you are on is 6 85
+enter r or qr
+dice number and the number you are on is 2 87
+enter r or qr
+dice number and the number you are on is 2 89
+u r caught by a snake, go down
+enter r or qr
+dice number and the number you are on is 6 76
+u have got a ladder, clim it up
+enter r or qr
+dice number and the number you are on is 1 98
+enter r or qr
+dice number and the number you are on is 6 104
+should get valid number to reach 100
+enter r or qr
+dice number and the number you are on is 5 103
+should get valid number to reach 100
+enter r or qr
+dice number and the number you are on is 5 103
+should get valid number to reach 100
+enter r or qr
+dice number and the number you are on is 5 103
+should get valid number to reach 100
+enter r or qr
+dice number and the number you are on is 3 101
+should get valid number to reach 100
+enter r or qr
+dice number and the number you are on is 4 102
+should get valid number to reach 100
+enter r or qr
+dice number and the number you are on is 6 104
+should get valid number to reach 100
+enter r or qr
+dice number and the number you are on is 3 101
+should get valid number to reach 100
+enter r or qr
+dice number and the number you are on is 2 100
+congrats, u r the winner
