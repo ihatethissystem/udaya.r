855a7de71e172f88acbaaeb22797b1c5bd77cd1f
ihatethissystem <43032279+ihatethissystem@users.noreply.github.com>
2018-09-29 09:43:08 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,35 @@
+dl320@soetcse:~/udaya$ python3 function1.py
+dl320@soetcse:~/udaya$ python3 function1.py
+300
+dl320@soetcse:~/udaya$ python3 function1.py
+-50
+dl320@soetcse:~/udaya$ python3 function1.py
+30000
+dl320@soetcse:~/udaya$ python3 function1.py
+0.75
+dl320@soetcse:~/udaya$ python3 function1.py
+0.75
+dl320@soetcse:~/udaya$ python3 function1.py
+30000
+dl320@soetcse:~/udaya$ python3 function1.py
+-50
+dl320@soetcse:~/udaya$ python3 function1.py
+350
+
+
+
+dl320@soetcse:~/udaya$ python3 function1.py
+enter number A:45
+enter the number B:76
+multiple: 121
+
+
+
+dl320@soetcse:~/udaya$ python3 function1.py
+enter number A:45
+enter the number B:54
+addition: 99
+subtraction: -9
+
+
+
